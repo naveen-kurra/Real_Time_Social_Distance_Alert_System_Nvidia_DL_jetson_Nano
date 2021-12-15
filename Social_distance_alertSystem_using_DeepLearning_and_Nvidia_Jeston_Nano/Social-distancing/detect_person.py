ce376e9c0621fec63fabc46771b6e42d2505aa44
Naveen Kurra <70699929+naveen-kurra@users.noreply.github.com>
2021-12-15 17:20:50 +0530

    Add files via upload

---
+++ Social_distance_alertSystem_using_DeepLearning_and_Nvidia_Jeston_Nano/Social-distancing/detect_person.py
@@ -0,0 +1,197 @@
+from scipy.spatial import distance as dist
+import numpy as np
+import imutils
+import cv2
+import os
+import playsound
+import time
+#import torch
+#print(cv2.__version__)
+
+# base path to YOLO directory
+MODEL_PATH = "yolov4-coco"
+prev_frame_time = 0
+display=1
+
+# used to record the time at which we processed current frame
+new_frame_time = 0
+
+# initialize minimum probability to filter weak detections along with
+# the threshold when applying non-maxima suppression
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+
+# boolean indicating if NVIDIA CUDA GPU should be used
+USE_GPU = True
+
+# define the minimum safe distance (in pixels) that two people can be
+# from each other
+MIN_DISTANCE=5000
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join([MODEL_PATH, "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+print(LABELS)
+
+print(len(LABELS))
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join([MODEL_PATH, "yolov4.weights"])
+configPath = os.path.sep.join([MODEL_PATH, "yolov4.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+# check if we are going to use GPU
+if USE_GPU:
+    # set CUDA as the preferable backend and target
+    print("Setting preferable backend and target to CUDA...")
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
+
+src = cv2.cuda_GpuMat()
+
+# determine only the *output* layer names that we need from YOLO
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+print("Accessing video stream...")
+#'rtsp://admin:admin@172.18.31.201/?langinfo=1'
+#upload the video file you want to check social distancing for- below.
+vs = cv2.VideoCapture('rtsp://admin:admin@172.18.31.201/?langinfo=1')
+writer = None
+
+def detect_people(frame, net, ln, personIdx=0):
+    # grab the dimensions of the frame and  initialize the list of
+    # results
+    #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    (H, W) = frame.shape[:2]
+    results = []
+
+    # construct a blob from the input frame and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes
+    # and associated probabilities
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
+    net.setInput(blob)
+    layerOutputs = net.forward(ln)
+
+    # initialize our lists of detected bounding boxes, centroids, and
+    # confidences, respectively
+    boxes = []
+    centroids = []
+    confidences = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            # filter detections by (1) ensuring that the object
+            # detected was a person and (2) that the minimum
+            # confidence is met
+            if classID == personIdx and confidence > MIN_CONF:
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates,
+                # centroids, and confidences
+                boxes.append([x, y, int(width), int(height)])
+                centroids.append((centerX, centerY))
+                confidences.append(float(confidence))
+
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, MIN_CONF, NMS_THRESH)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # update our results list to consist of the person
+            # prediction probability, bounding box coordinates,
+            # and the centroid
+            r = (confidences[i], (x, y, x + w, y + h), centroids[i])
+            results.append(r)
+
+    # return the list of results
+    return results
+
+display = 1
+while True:
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
+
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
+
+    # resize the frame and then detect people (and only people) in it
+    frame = imutils.resize(frame, width=700)
+    results = detect_people(frame, net, ln, personIdx=LABELS.index("person"))
+
+    for (i, (prob, bbox, centroid)) in enumerate(results):
+        # extract the bounding box and centroid coordinates, then
+        # initialize the color of the annotation
+        (startX, startY, endX, endY) = bbox
+        (cX, cY) = centroid
+        color = (0, 255, 0)
+
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+        cv2.circle(frame, (cX, cY), 5, color, 1)
+
+    if display > 0:
+        # show the output frame
+        # resizing the frame size according to our need
+        gray=frame
+        gray = cv2.resize(gray, (500, 300))
+
+        # font which we will be using to display FPS
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        # time when we finish processing for this frame
+        new_frame_time = time.time()
+
+        # Calculating the fps
+
+        # fps will be number of frame processed in given time frame
+        # since their will be most of time error of 0.001 second
+        # we will be subtracting it to get more accurate result
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+
+        # converting the fps into integer
+        fps = int(fps)
+
+        # converting the fps to string so that we can display it on frame
+        # by using putText function
+        fps = str(fps)
+
+        # putting the FPS count on the frame
+        cv2.putText(frame, fps, (7, 70), font, 3, (100, 255, 0), 3, cv2.LINE_AA)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
